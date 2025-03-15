3064a5e1f32c335ed0f3b845d8af79a0011c9e52
JavaFullStack1 <santoshvtucse@gmail.com>
2025-03-15 15:20:51 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test5Application {
-
 	public static void main(String[] args) {
+		
+		String x = "Mike";
 		SpringApplication.run(Test5Application.class, args);
 	}
 
