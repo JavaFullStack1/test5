26f57b855f96a1b719a42ee96c2dc520a24ab32f
santoshluhar <santoshvtucse@gmail.com>
2025-03-15 15:24:09 +0530

    Second Commit

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test5Application {
-
 	public static void main(String[] args) {
+
+		int x =  100;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
